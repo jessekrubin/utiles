4a547de004a2d9b0f8b5216ac27fceb294a37e39
jessekrubin <jessekrubin@gmail.com>
2023-11-10 13:33:01 -0800

    fix cicd?

--- crates/utiles-cli/src/cli.rs
+++ crates/utiles-cli/src/cli.rs
@@ -49,7 +49,7 @@ pub struct InputAndSequenceArgs {
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
-    #[command(name = "lint", about = "lint mbtiles file", long_about = None)]
+    #[command(name = "lint", about = "Lint mbtiles file(s)", long_about = None)]
     Lint {
         #[arg(required = true)]
         filepath: String,
@@ -57,7 +57,7 @@ pub enum Commands {
         #[arg(required = false, long, action = clap::ArgAction::SetTrue)]
         fix: bool,
     },
-    #[command(name = "tilejson", visible_alias = "tj", about = "echo tilejson", long_about = None)]
+    #[command(name = "tilejson", visible_alias = "tj", about = "Echo tileson for mbtiles file(s)", long_about = None)]
     Tilejson {
         #[arg(required = true, help = "mbtiles filepath")]
         filepath: String,
@@ -66,7 +66,7 @@ pub enum Commands {
         min: bool,
     },
 
-    #[command(name = "meta", about = "echo metadata (table) as json", long_about = None)]
+    #[command(name = "meta", about = "Echo metadata (table) as json", long_about = None)]
     Meta {
         #[arg(required = true, help = "mbtiles filepath")]
         filepath: String,
@@ -80,7 +80,7 @@ pub enum Commands {
     // ========================================================================
     // TILE CLI UTILS - MERCANTILE LIKE CLI
     // ========================================================================
-    #[command(name = "tiles", about = "echo tiles of bbox", long_about = None)]
+    #[command(name = "tiles", about = "Echo tiles of bbox", long_about = None)]
     Tiles {
         #[arg(required = true)]
         zoom: u8,
@@ -92,13 +92,13 @@ pub enum Commands {
         seq: bool,
     },
 
-    #[command(name = "quadkey", visible_alias = "qk", about = "convert xyz <-> quadkey", long_about = None)]
+    #[command(name = "quadkey", visible_alias = "qk", about = "Convert to/from quadkey(s)", long_about = None)]
     Quadkey {
         #[arg(required = false)]
         input: Option<String>,
     },
 
-    #[command(name = "bounding-tile", about = "output tilejson", long_about = None)]
+    #[command(name = "bounding-tile", about = "Echo the bounding tile of a lonlat/bbox/GeoJSON", long_about = None)]
     BoundingTile {
         #[arg(required = false)]
         input: Option<String>,
@@ -106,7 +106,7 @@ pub enum Commands {
         #[arg(required = false, long, action = clap::ArgAction::SetTrue)]
         seq: bool,
     },
-    #[command(name = "neighbors", about = "echo neighbors of tile(s)", long_about = None)]
+    #[command(name = "neighbors", about = "Echo neighbors of tile(s)", long_about = None)]
     Neighbors {
         #[arg(required = false)]
         input: Option<String>,
@@ -115,7 +115,7 @@ pub enum Commands {
         seq: bool,
     },
 
-    #[command(name = "parent", about = "echo parent of tile(s)", long_about = None)]
+    #[command(name = "parent", about = "Echo parent of tile(s)", long_about = None)]
     Parent {
         #[arg(required = false)]
         input: Option<String>,
@@ -126,7 +126,7 @@ pub enum Commands {
         #[arg(required = false, long, default_value = "1")]
         depth: u8,
     },
-    #[command(name = "children", about = "echo children of tile(s)", long_about = None)]
+    #[command(name = "children", about = "Echo children of tile(s)", long_about = None)]
     Children {
         #[arg(required = false)]
         input: Option<String>,
@@ -138,7 +138,7 @@ pub enum Commands {
         depth: u8,
     },
 
-    #[command(name = "shapes", about = "echo shapes of tile(s) as GeoJSON", long_about = None)]
+    #[command(name = "shapes", about = "Echo shapes of tile(s) as GeoJSON", long_about = None)]
     Shapes(ShapesArgs),
 }
 
