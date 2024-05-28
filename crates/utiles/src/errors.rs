779da3454cd6bb0c613811a209b11dad9eecd93d
jesse <jessekrubin@gmail.com>
2024-05-28 16:54:27 -0700

    error docs

--- crates/utiles/src/errors.rs
+++ crates/utiles/src/errors.rs
@@ -6,9 +6,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum UtilesError {
-    #[error("utiles-core error: {0}")]
-    CoreError(#[from] utiles_core::UtilesCoreError),
-
     #[error("Unimplemented: {0}")]
     Unimplemented(String),
 
@@ -36,18 +33,30 @@ pub enum UtilesError {
     #[error("unknown utiles error: {0}")]
     Unknown(String),
 
+    // ===============================================================
+    // EXTERNAL ~ EXTERNAL ~ EXTERNAL ~ EXTERNAL ~ EXTERNAL ~ EXTERNAL
+    // ===============================================================
+    /// Error from the utiles-core crate
+    #[error("utiles-core error: {0}")]
+    CoreError(#[from] utiles_core::UtilesCoreError),
+
+    /// Error from std::io
     #[error("io error: {0}")]
     IoError(#[from] std::io::Error),
 
+    /// Error from rusqlite
     #[error("sqlite err: {0}")]
     SqliteError(#[from] rusqlite::Error),
 
+    /// Error from async_sqlite
     #[error("sqlite err: {0}")]
     AsyncSqliteError(#[from] async_sqlite::Error),
 
+    /// Error from globset
     #[error("globset error: {0}")]
     GlobsetError(#[from] globset::Error),
 
+    /// Error from serde_json
     #[error("serde error: {0}")]
     SerdeError(#[from] serde_json::Error),
 }
