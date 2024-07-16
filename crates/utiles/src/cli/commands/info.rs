305d2e038edc1471c5142ed6cb4fd93f540c94eb
jesse <jessekrubin@gmail.com>
2024-07-16 14:23:58 -0700

    info all async!

--- crates/utiles/src/cli/commands/info.rs
+++ crates/utiles/src/cli/commands/info.rs
@@ -10,16 +10,16 @@ use crate::utilesqlite::{MbtilesAsync, MbtilesAsyncSqliteClient};
 async fn mbinfo(filepath: &str) -> UtilesResult<MbtilesStats> {
     let fspath = Path::new(filepath);
     let is_file = file_exists(filepath).await;
-    if !is_file {
+    if is_file {
+        let mbt = MbtilesAsyncSqliteClient::open_existing(filepath).await?;
+        let stats = mbt.mbt_stats(None).await?;
+        Ok(stats)
+    } else {
         let abspath = canonicalize(fspath)?;
 
         Err(crate::errors::UtilesError::NotAFile(
             abspath.to_string_lossy().to_string(),
         ))
-    } else {
-        let mbt = MbtilesAsyncSqliteClient::open_existing(filepath).await?;
-        let stats = mbt.mbt_stats(None).await?;
-        Ok(stats)
     }
 }
 
