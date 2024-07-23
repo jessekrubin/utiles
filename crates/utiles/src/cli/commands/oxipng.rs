de92b0f375f1ded2a7c65d3c87ab5f19d539a909
jesse <jessekrubin@gmail.com>
2024-07-23 13:57:18 -0700

    Progress (#122)
    * oxipng v1
    
    * clippy lints
    
    * push for merge
    
    * oxipng progress
    
    * progress
    
    * ah remove bin feature from oxipng
    
    * install clang in cicd?
    
    * oh it is rhel?
    
    * ok install clang on correct machines
    
    * oh the problem children are debian based
    
    * well i dont use those target os...
    
    * fix webpify flipped
    
    * fix webpify

--- crates/utiles/src/cli/commands/oxipng.rs
+++ crates/utiles/src/cli/commands/oxipng.rs
@@ -1,18 +1,22 @@
-use futures::StreamExt;
-use tokio::join;
-use tokio_stream::wrappers::ReceiverStream;
-use tracing::{debug, info, warn};
-
 use crate::cli::args::OxipngArgs;
 use crate::img::oxipngify;
 use crate::mbt::{make_tiles_stream, MbtStreamWriter, MbtWriterStats};
 use crate::utilesqlite::{Mbtiles, MbtilesAsync, MbtilesAsyncSqliteClient};
 use crate::UtilesResult;
+use futures::StreamExt;
+use indicatif;
+use indicatif::ProgressStyle;
+use std::time::Duration;
+use tokio::join;
+use tokio_stream::wrappers::ReceiverStream;
+use tracing::{debug, info, warn};
 
 pub async fn oxipng_main(args: OxipngArgs) -> UtilesResult<()> {
     let mbt =
         MbtilesAsyncSqliteClient::open_existing(args.common.filepath.as_str()).await?;
     mbt.assert_mbtiles().await?;
+
+    let total_count = mbt.tiles_count().await?;
 
     let mbt_metadata = mbt.metadata_rows().await?;
     let dst_mbtiles = Mbtiles::open_new(args.dst, None)?;
@@ -26,12 +30,16 @@ pub async fn oxipng_main(args: OxipngArgs) -> UtilesResult<()> {
         mbt: dst_mbtiles,
         stats: MbtWriterStats::default(),
     };
+    let jobs: usize = args.jobs.unwrap_or(4) as usize;
+
+    let (tx_progress, mut rx_progress) = tokio::sync::mpsc::channel(100);
 
     let proc_future = tokio::spawn(async move {
         // TODO: cli flag for concurrency
         tiles_stream
-            .for_each_concurrent(4, |(tile, tile_data)| {
+            .for_each_concurrent(jobs, |(tile, tile_data)| {
                 let tx_writer = tx_writer.clone();
+                let tx_progress = tx_progress.clone();
                 let oxipng_options = oxipng::Options::from_preset(args.opt);
                 async move {
                     let initial_size = tile_data.len();
@@ -46,13 +54,15 @@ pub async fn oxipng_main(args: OxipngArgs) -> UtilesResult<()> {
                         Ok(oxipngify_res) => match oxipngify_res {
                             Ok(img_result) => {
                                 let final_size = img_result.len();
-                                let size_diff: i32 =
-                                    initial_size as i32 - final_size as i32;
+                                let size_diff = initial_size - final_size;
                                 debug!("size_diff: {}", size_diff);
                                 let send_res = tx_writer.send((tile, img_result)).await;
 
                                 if let Err(e) = send_res {
                                     warn!("send_res: {:?}", e);
+                                } else if let Err(e) = tx_progress.send(size_diff).await
+                                {
+                                    warn!("send error tx_progress: {:?}", e);
                                 }
                             }
                             Err(e) => {
@@ -65,10 +75,43 @@ pub async fn oxipng_main(args: OxipngArgs) -> UtilesResult<()> {
             .await;
     });
 
-    let (result, writer_result) = join!(proc_future, writer.write_batched());
+    let progress_future = tokio::spawn(async move {
+        let mut total_size_diff = 0;
+        let mut processed = 0;
+        let pb = indicatif::ProgressBar::new(total_count as u64);
+        pb.set_message("oxipng-ing");
+        let pb_style = ProgressStyle::with_template(
+            "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+        );
+        match pb_style {
+            Err(e) => {
+                warn!("pb_style error: {:?}", e);
+            }
+            Ok(s) => {
+                pb.set_style(s);
+            }
+        }
+        pb.enable_steady_tick(Duration::from_millis(100));
+        while let Some(size_diff) = rx_progress.recv().await {
+            total_size_diff += size_diff;
+            processed += 1;
+            pb.inc(1);
+
+            let size_saved = size::Size::from_bytes(total_size_diff as u64).to_string();
+            pb.set_message(format!("size-diff: {size_saved}"));
+        }
+        let total_size_str = size::Size::from_bytes(total_size_diff as u64).to_string();
+        pb.finish_with_message(format!(
+            "Processed {processed} tiles, saved {total_size_str} ({total_size_diff}b)"
+        ));
+    });
+
+    let (cruncher_res, writer_res, progress_res) =
+        join!(proc_future, writer.write_batched(), progress_future);
     let elapsed = start_time.elapsed();
     info!("elapsed: {:?}", elapsed);
-    result?;
-    writer_result?;
+    cruncher_res?;
+    writer_res?;
+    progress_res?;
     Ok(())
 }
--- crates/utiles/src/cli/commands/webpify.rs
+++ crates/utiles/src/cli/commands/webpify.rs
@@ -1,4 +1,6 @@
 use futures::StreamExt;
+use indicatif::ProgressStyle;
+use std::time::Duration;
 use tokio::join;
 use tokio_stream::wrappers::ReceiverStream;
 use tracing::{info, warn};
@@ -13,13 +15,14 @@ pub async fn webpify_main(args: WebpifyArgs) -> UtilesResult<()> {
     let mbt =
         MbtilesAsyncSqliteClient::open_existing(args.common.filepath.as_str()).await?;
     mbt.assert_mbtiles().await?;
+    let total_count = mbt.tiles_count().await?;
 
     let mbt_metadata = mbt.metadata_rows().await?;
     let dst_mbtiles = Mbtiles::open_new(args.dst, None)?;
     dst_mbtiles.metadata_set_many(&mbt_metadata)?;
     dst_mbtiles.metadata_set("format", "webp")?;
     let tiles_stream = make_tiles_stream(&mbt)?;
-
+    let (tx_progress, mut rx_progress) = tokio::sync::mpsc::channel(100);
     let (tx_writer, rx_writer) = tokio::sync::mpsc::channel(100);
     let start_time = std::time::Instant::now();
     let mut writer = MbtStreamWriter {
@@ -32,6 +35,9 @@ pub async fn webpify_main(args: WebpifyArgs) -> UtilesResult<()> {
         tiles_stream
             .for_each_concurrent(4, |(tile, tile_data)| {
                 let tx_writer = tx_writer.clone();
+                let tx_progress = tx_progress.clone();
+                let initial_size = tile_data.len();
+
                 async move {
                     let blocking_res =
                         tokio::task::spawn_blocking(move || webpify_image(&tile_data))
@@ -42,9 +48,14 @@ pub async fn webpify_main(args: WebpifyArgs) -> UtilesResult<()> {
                         }
                         Ok(webpify_result) => match webpify_result {
                             Ok(webp_bytes) => {
+                                let size_diff = initial_size - webp_bytes.len();
                                 let send_res = tx_writer.send((tile, webp_bytes)).await;
                                 if let Err(e) = send_res {
                                     warn!("send_res: {:?}", e);
+                                }
+                                let send_res = tx_progress.send(size_diff).await;
+                                if let Err(e) = send_res {
+                                    warn!("progress send_res: {:?}", e);
                                 }
                             }
                             Err(e) => {
@@ -57,10 +68,43 @@ pub async fn webpify_main(args: WebpifyArgs) -> UtilesResult<()> {
             .await;
     });
 
-    let (result, writer_result) = join!(proc_future, writer.write());
+    let progress_future = tokio::spawn(async move {
+        let mut total_size_diff = 0;
+        let mut processed = 0;
+        let pb = indicatif::ProgressBar::new(total_count as u64);
+        pb.set_message("webpify");
+        let pb_style = ProgressStyle::with_template(
+            "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+        );
+        match pb_style {
+            Err(e) => {
+                warn!("pb_style error: {:?}", e);
+            }
+            Ok(s) => {
+                pb.set_style(s);
+            }
+        }
+        pb.enable_steady_tick(Duration::from_millis(100));
+        while let Some(size_diff) = rx_progress.recv().await {
+            total_size_diff += size_diff;
+            processed += 1;
+            pb.inc(1);
+
+            let size_saved = size::Size::from_bytes(total_size_diff as u64).to_string();
+            pb.set_message(format!("webpify ~ size-diff: {size_saved}"));
+        }
+        let total_size_str = size::Size::from_bytes(total_size_diff as u64).to_string();
+        pb.finish_with_message(format!(
+            "Processed {processed} tiles, saved {total_size_str} ({total_size_diff}b)"
+        ));
+    });
+    let (result, writer_result, progress_res) =
+        join!(proc_future, writer.write(), progress_future);
     let elapsed = start_time.elapsed();
     info!("elapsed: {:?}", elapsed);
     result?;
     writer_result?;
+    progress_res?;
+
     Ok(())
 }
