3885009d1bf5d633dc05db8b5e266786fbaa242c
jessekrubin <jessekrubin@gmail.com>
2025-07-02 17:11:03 -0700

    fmt

--- crates/utiles/src/dev/sqlite_u64.rs
+++ crates/utiles/src/dev/sqlite_u64.rs
@@ -23,7 +23,7 @@ pub fn i64_to_u64_shift(value: i64) -> u64 {
 #[must_use]
 #[allow(unsafe_code)]
 pub fn u64_to_i64_unsafe_transmute(value: u64) -> i64 {
-     u64::cast_signed(value)
+    u64::cast_signed(value)
 }
 
 #[must_use]
