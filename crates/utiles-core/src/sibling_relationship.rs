c874b332c2d6d86f8067b3610f24e86190bdb45d
jessekrubin <jessekrubin@gmail.com>
2025-09-22 12:16:31 -0700

    clippy fixes

--- crates/utiles-core/src/sibling_relationship.rs
+++ crates/utiles-core/src/sibling_relationship.rs
@@ -15,8 +15,8 @@ pub enum SiblingRelationship {
 
 impl From<(u32, u32)> for SiblingRelationship {
     fn from(value: (u32, u32)) -> Self {
-        let is_left = value.0 % 2 == 0;
-        let is_top = value.1 % 2 == 0;
+        let is_left = value.0.is_multiple_of(2);
+        let is_top = value.1.is_multiple_of(2);
         match (is_left, is_top) {
             (true, true) => Self::UpperLeft,
             (true, false) => Self::LowerLeft,
@@ -28,8 +28,8 @@ impl From<(u32, u32)> for SiblingRelationship {
 
 impl From<Tile> for SiblingRelationship {
     fn from(value: Tile) -> Self {
-        let is_left = value.x % 2 == 0;
-        let is_top = value.y % 2 == 0;
+        let is_left = value.x.is_multiple_of(2);
+        let is_top = value.y.is_multiple_of(2);
         match (is_left, is_top) {
             (true, true) => Self::UpperLeft,
             (true, false) => Self::LowerLeft,
