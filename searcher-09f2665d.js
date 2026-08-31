f05656b9aafc54a9703dcd74e7e8e79c41577dbd
jessekrubin <jessekrubin@users.noreply.github.com>
2026-08-31 16:52:21 +0000

    deploy: 31bd0f812e3923708087e9d9d805056ec875479d

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2cd6c7bd.js',
+                path_to_root + 'searchindex-2c1293bd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
